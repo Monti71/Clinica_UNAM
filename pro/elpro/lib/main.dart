1625fa75d59c021fc80b40db36aa1b84d23d8058
Ian Marentes <ianmarentes@MacBook-Air-de-Ian.local>
2019-10-27 10:16:23 -0600

    Se agrega archivos Ian

---
+++ pro/elpro/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:elpro/src/app.dart';
+import 'package:flutter/material.dart';
+//import 'src/app.dart';
+//import 'package:contador/src/app.dart';
+
+void main() {
+  runApp( MyApp() );
+}
